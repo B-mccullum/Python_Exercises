fa4d6ecc304e0d137edfac9ea5a5221edbb12aa4
B-mccullum <bmc2994@yahoo.com>
2023-06-05 18:12:51 +0000

    Updated

--- Delete_object.py
+++ Delete_object.py
@@ -1,9 +1,31 @@
 import boto3
 
+
+def delete_object(client, bucket, key):
+    response = client.delete_object(
+        Bucket=bucket, 
+        Key=key
+        )
+    return response
+
+def delete_objects(client, buckets, keys):
+    objects = [{'Key': key} for key in keys]
+     
+  
+    response = client.delete_objects(
+        Bucket=bucket, 
+        Delete={
+            'Objects':objects
+        }
+    )
+    
+    return response
+        
 bucket = "bmccullum-boto3-06022023"
 key = "test_text.txt"
 
 s3= boto3.client('s3')
 
-response = s3.delete_object(Bucket=bucket, Key=key,)
- 
+keys = ["test_text_string.txt", "test_text_upload.txt"]
+
+delete_objects(s3, bucket, keys)
--- Upload_new_object.py
+++ Upload_new_object.py
@@ -2,7 +2,10 @@ import boto3
 
 s3 = boto3.client('s3')
 
-s3.put_object(Bucket="bmccullum-boto3-06022023", Key="test_text_string.txt" , Body="Hey this is a string", ContentType="text/plain")
+s3.put_object(Bucket="bmccullum-boto3-06022023", 
+                Key="folder/foldera/folder1/test_text_string.txt" , 
+                Body="Hey this is a string", 
+                ContentType="text/plain")
   
 
 
