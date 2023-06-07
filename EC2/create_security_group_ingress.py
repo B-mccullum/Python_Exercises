73283544002fd144af14ae88576ae4cdfa7b0865
B-mccullum <bmc2994@yahoo.com>
2023-06-07 13:09:20 +0000

    Create Security group and Ingress

---
+++ EC2/create_security_group_ingress.py
@@ -0,0 +1,33 @@
+import boto3 
+
+security_group_id = "sg-046d6752cc338a2e6"
+ 
+ec2 = boto3.client('ec2')
+
+response = ec2.authorize_security_group_ingress(
+    GroupId=security_group_id,
+    IpPermissions=[
+        {
+            'FromPort': 22,
+            'IpProtocol': 'tcp',
+            'IpRanges': [
+                {
+                    'CidrIp': '0.0.0.0/0',
+                },
+            ],
+            'ToPort': 22,
+        },
+        {
+            'FromPort': 80,
+            'IpProtocol': 'tcp',
+            'IpRanges': [
+                {
+                    'CidrIp': '0.0.0.0/0',
+                },
+            ],
+            'ToPort': 80,
+        },
+    ],
+)
+
+print(response)
