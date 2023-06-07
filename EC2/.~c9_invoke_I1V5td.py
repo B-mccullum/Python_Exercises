32a69b160de81045b73066170a75b262e34a0521
Brandon McCullum <129446092+B-mccullum@users.noreply.github.com>
2023-06-06 20:05:51 -0500

    Delete .~c9_invoke_I1V5td.py

--- EC2/.~c9_invoke_I1V5td.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-ec2 = boto3.client('ec2')
-
-response = ec2.describe_images(Owners=['self'])
-
-for image in response["Images"]:
-    print(image["ImageId"], image["Name"], image["CreationDate"])
-    
